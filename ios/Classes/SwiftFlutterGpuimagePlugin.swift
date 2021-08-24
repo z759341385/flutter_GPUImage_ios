eaaf244d6d07394b8be4db61b369ee5d0a5c2720
z759341385 <39547947+z759341385@users.noreply.github.com>
2021-08-24 09:35:09 +0800

    1
    gpu image3 替换

--- ios/Classes/SwiftFlutterGpuimagePlugin.swift
+++ ios/Classes/SwiftFlutterGpuimagePlugin.swift
@@ -12,91 +12,59 @@ public class SwiftFlutterGpuimagePlugin: NSObject, FlutterPlugin {
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     
     let arguments = call.arguments as? [String: Any] ?? [:]
+    
     if call.method == "progressImage" {
         
-        guard let sourceImageData = arguments["sourceImage"] as? FlutterStandardTypedData, let sourceImage = UIImage(data: sourceImageData.data), let sourceImageSource = GPUImagePicture(image: sourceImage), let filtersJSON = arguments["filters"] as? [[String: Any]] else {
+        guard let sourceImageData = arguments["sourceImage"] as? FlutterStandardTypedData, let sourceImage = UIImage(data: sourceImageData.data), let filtersJSON = arguments["filters"] as? [[String: Any]] else {
             result(nil)
             return
         }
         
-        let filterGroup = GPUImageFilterGroup()
+        let sourceImageSource = PictureInput(image: sourceImage)
         
-        sourceImageSource.addTarget(filterGroup)
+        let operationGroup = OperationGroup()
+        
+        var filters: [BasicOperation] = []
         
         for dic in filtersJSON {
             let name = dic["name"] as? String ?? ""
             let data = dic["data"] as? [String: Any] ?? [:]
             if(name == "GPUImageLookupFilter") {
                 let lookupImageData = Data(data["filterImage"] as? [UInt8] ?? [])
-                guard let lookupImage = UIImage(data: lookupImageData), let lookupImageSource = GPUImagePicture(image: lookupImage) else {
+                guard let lookupImage = UIImage(data: lookupImageData) else {
                     break
                 }
-                let lookupFilter = GPUImageLookupFilter()
+                let lookupImageSource = PictureInput(image: lookupImage)
                 
-                filterGroup.addFilter(lookupFilter)
+                let lookupFilter = LookupFilter()
+                lookupFilter.lookupImage = lookupImageSource
                 
-                sourceImageSource.addTarget(lookupFilter)
-                sourceImageSource.processImage()
-                
-                lookupImageSource.addTarget(lookupFilter)
-                lookupImageSource.processImage()
-                
-
+                filters.append(lookupFilter)
             }
         }
         
-        let filterCount = filterGroup.filterCount()
-        
-        
-        if filterCount > 1 {
-            for index in 1..<filterCount {
-                let frontFilter = filterGroup.filter(at: index - 1)
-                let filter = filterGroup.filter(at: index)
-                frontFilter?.addTarget(filter)
+        operationGroup.configureGroup { input, output in
+            for(index, filter) in filters.enumerated() {
+                if index == 0 {
+                    input.addTarget(filter)
+                }
+                if filters.count > 1 {
+                    filters[index - 1].addTarget(filter)
+                }
+                if index == filters.count - 1 {
+                    filter.addTarget(output)
+                }
             }
         }
         
-        if filterCount > 0, let first = filterGroup.filter(at: 0) {
-            filterGroup.initialFilters = [ first ]
-        }
+        let resultImage = sourceImage.filterWithOperation(operationGroup)                
         
-        if filterCount > 0, let last = filterGroup.filter(at: filterCount - 1) {
-            filterGroup.terminalFilter = last
-        }
-        
-        sourceImageSource.processImage()
-        
-        filterGroup.useNextFrameForImageCapture()
-        
-        guard let resultImage = filterGroup.imageFromCurrentFramebuffer(), let data = resultImage.jpegData(compressionQuality: 1.0) else {
+        guard let data = resultImage.jpegData(compressionQuality: 1.0) else {
             result(nil)
             return
         }
         
         result(FlutterStandardTypedData(bytes: data))
-        
-        
-//
-//        let lookupImageData = dic["lookupImage"] as? FlutterStandardTypedData
-//        let lookupImageGP = GPUImagePicture(image: lookupImage)
-//
-        
-//        let lookupFilter = GPUImageLookupFilter()
-        
-//        sourceImageGP.addTarget(lookupFilter)
-//        lookupImageGP.addTarget(lookupFilter)
-        
-//        sourceImageGP.processImage()
-//        lookupImageGP.processImage()
-        
-//        lookupFilter.useNextFrameForImageCapture()
-//
-//        guard let resultImage = lookupFilter.imageFromCurrentFramebuffer(), let data = resultImage.jpegData(compressionQuality: 1.0) else {
-//            result(nil)
-//            return
-//        }
-//
-//        result(FlutterStandardTypedData(bytes: data))
     } else {
         result("iOS " + UIDevice.current.systemVersion)
     }
