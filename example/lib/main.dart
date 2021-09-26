5623ee2ae01d257dba35ce0bc9ef7cc0a120a48f
Zhanghongchen <zhanghongchen1998@126.com>
2021-09-26 10:04:37 +0800

    BBMetalImage

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,67 +4,83 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_gpuimage/flutter_gpuimage.dart';
 
+import 'package:flutter/material.dart';
+
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  Uint8List _image;
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Column(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Uint8List _image;
+
+  @override
+  void initState() {
+    super.initState();
+    rootBundle.load('assets/images/1351631461463.jpg').then((value) {
+      setState(() {
+        _image = value.buffer.asUint8List();
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            _image == null ? SizedBox.shrink() : Expanded(
+              child: Image.memory(
+                _image,
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.fill,
+              ),
+            ),
             FlatButton(
                 onPressed: () async {
-                  final sourceImage =
-                      await rootBundle.load('assets/images/1629819276523.jpg');
-                  // final sourceImage =
-                  //     await rootBundle.load('assets/images/IMG_0945.JPG');
-
                   final lookupImage =
-                      await rootBundle.load('assets/images/lookup1.jpg');
+                      await rootBundle.load('assets/images/lookup-table-pta.png');
                   final lookupFilter = GPUImageLookupFilter(
                       filterImage: lookupImage.buffer.asUint8List());
                   final result = await FlutterGpuimage.progressImage(
-                      sourceImage: sourceImage.buffer.asUint8List(),
+                      sourceImage: _image,
                       filters: [lookupFilter]);
                   setState(() {
                     _image = result;
                   });
                 },
                 child: Text(
-                  '图片',
+                  '添加滤镜',
                   style: TextStyle(color: Colors.purple),
                 )),
-            _image == null
-                ? Container(
-                    width: 200,
-                    height: 200,
-                    color: Colors.deepOrange,
-                  )
-                : Image.memory(
-                    _image,
-                    width: 200,
-                    height: 200,
-                  ),
           ],
         ),
       ),
